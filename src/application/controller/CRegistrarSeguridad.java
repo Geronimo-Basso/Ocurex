90351d9c4f66b6279add74975883308619584428
Geronimo Basso <gerobasso01@gmail.com>
2023-01-15 20:55:22 +0100

    Change the users and deprecate the class Usuario

--- src/application/controller/CRegistrarSeguridad.java
+++ src/application/controller/CRegistrarSeguridad.java
@@ -9,6 +9,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Type;
+//import java.util.ArrayList;
+import java.util.Vector;
+
+//import java.sql.Timestamp;
+//import java.util.Date;
+
 
 public class CRegistrarSeguridad {
 
@@ -99,6 +113,5 @@ public class CRegistrarSeguridad {
 			e.printStackTrace();
 		}
 		//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    }	
-    	
+    } 	
 }
