7eab428f81e932f1b6804446d8784e6ae205ab14
Geronimo Basso <gerobasso01@gmail.com>
2023-01-21 01:18:05 +0100

    Basic checks in the Registrar Doctor and Registrar seguridad

--- src/application/controlador/CRegistrarDoctor.java
+++ src/application/controlador/CRegistrarDoctor.java
@@ -11,6 +11,7 @@ import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 
+import application.controlador.excepciones.DatosVacios;
 import application.model.Director;
 import application.model.Medico;
 import javafx.event.ActionEvent;
@@ -98,70 +99,80 @@ public class CRegistrarDoctor {
        }
     
     @FXML
-    void registrarse(ActionEvent event) {
-    	
-    	String email = jfxEmail.getText();
-    	String nombre = jfxNombre.getText();
-    	String apellido = jfxApellido.getText();
-    	String password = jfxPassword.getText();
-    	int telefono = Util.parsearInt(jfxTelefono.getText());
-    	String sexo = jfxSexo.getText();
-    	String domicilio = jfxDomicilio.getText();
-    	String especialidad = jfxEspecialidad.getText();
-
-
-		Medico medico = new Medico( email , nombre , apellido , password ,especialidad , telefono , domicilio , sexo , LocalDate.now());
-		Medico medico1 = new Medico( "angel.charte@example.com" , "Angel" , "Chartel" , "ibuprofeno1" , "Medicina familiar" , 624785123 , "C/Principe Asturias 25, 28670" , "M" , LocalDate.now());
-		Medico medico2 = new Medico( "rafael.mur@example.com" , "Rafael" , "Mur" , "farmacia72" , "Traumatologia" , 658412784 , "C/Tajo 12, 28670" , "M" , LocalDate.now());
-		Medico medico3 = new Medico( "damian.olmo@example.com" , "Damian" , "Olmo" , "garganta79" , "Cirugia" , 632478652 , "C/San Francisco 22, 28670" , "M" , LocalDate.now());
-    	Vector<Medico> medicos = new Vector<Medico>();
-    	medicos.add(medico);
-    	medicos.add(medico1);
-    	medicos.add(medico2);
-    	medicos.add(medico3);
-		
-		Gson prettyGson = new GsonBuilder().setPrettyPrinting().create();
-		String representacionBonita = prettyGson.toJson(medicos);
-		System.out.println(representacionBonita);
-		
-		
-		try(FileWriter writer = new FileWriter("src/application/model/json/medicos.json")){
-			
-			prettyGson.toJson(medicos, writer);
-			
-	    } catch (IOException e) {
-	    	
-	        e.printStackTrace();
-	        
-	    }
+    void registrarse(ActionEvent event) throws DatosVacios {
+    	try {    	
+	    	String email = jfxEmail.getText();
+	    	String nombre = jfxNombre.getText();
+	    	String apellido = jfxApellido.getText();
+	    	String password = jfxPassword.getText();
+	    	int telefono = Util.parsearInt(jfxTelefono.getText());
+	    	String sexo = jfxSexo.getText();
+	    	String domicilio = jfxDomicilio.getText();
+	    	String especialidad = jfxEspecialidad.getText();
 	
+	    	if (Util.stringVacio(email) || Util.stringVacio(nombre) || Util.stringVacio(apellido) || Util.stringVacio(password) || Util.stringVacio(sexo) || Util.stringVacio(domicilio) || telefono == 0 || Util.stringVacio(especialidad)){
+		    	
+	    		throw new DatosVacios("Alguno de los datos esta mal");
+	    	}
 	    	
-    	Stage priorStage = (Stage)btnVolverAlMenu.getScene().getWindow();
-    	Stage stage = new Stage();
-
-    	try {
- 
-			FXMLLoader loader8 = new FXMLLoader(getClass().getResource("/application/view/IniciarSesion.fxml")); //Cargo el loader
+			Medico medico = new Medico( email , nombre , apellido , password ,especialidad , telefono , domicilio , sexo , LocalDate.now());
+			Medico medico1 = new Medico( "angel.charte@example.com" , "Angel" , "Chartel" , "ibuprofeno1" , "Medicina familiar" , 624785123 , "C/Principe Asturias 25, 28670" , "M" , LocalDate.now());
+			Medico medico2 = new Medico( "rafael.mur@example.com" , "Rafael" , "Mur" , "farmacia72" , "Traumatologia" , 658412784 , "C/Tajo 12, 28670" , "M" , LocalDate.now());
+			Medico medico3 = new Medico( "damian.olmo@example.com" , "Damian" , "Olmo" , "garganta79" , "Cirugia" , 632478652 , "C/San Francisco 22, 28670" , "M" , LocalDate.now());
+	    	Vector<Medico> medicos = new Vector<Medico>();
+	    	medicos.add(medico);
+	    	medicos.add(medico1);
+	    	medicos.add(medico2);
+	    	medicos.add(medico3);
 			
-			CIniciarSesion controlador8 = new CIniciarSesion(); //creo el controlador
+			Gson prettyGson = new GsonBuilder().setPrettyPrinting().create();
+			String representacionBonita = prettyGson.toJson(medicos);
+			System.out.println(representacionBonita);
+			
+			
+			try(FileWriter writer = new FileWriter("src/application/model/json/medicos.json")){
+				
+				prettyGson.toJson(medicos, writer);
+				
+		    } catch (IOException e) {
+		    	
+		        e.printStackTrace();
+		        
+		    }
 		
-			loader8.setController(controlador8); //seteo el controlador con el loader que cree antes.
+		    	
+	    	Stage priorStage = (Stage)btnVolverAlMenu.getScene().getWindow();
+	    	Stage stage = new Stage();
+	
+	    	try {
+	 
+				FXMLLoader loader8 = new FXMLLoader(getClass().getResource("/application/view/IniciarSesion.fxml")); //Cargo el loader
+				
+				CIniciarSesion controlador8 = new CIniciarSesion(); //creo el controlador
 			
-			Parent root8 = loader8.load(); //lo pongo como parent
-			
-			Scene scene = new Scene( root8 );
-			
-			stage.setScene(scene);
-			
-			stage.show();
-						
-			priorStage.close();	
+				loader8.setController(controlador8); //seteo el controlador con el loader que cree antes.
+				
+				Parent root8 = loader8.load(); //lo pongo como parent
+				
+				Scene scene = new Scene( root8 );
+				
+				stage.setScene(scene);
+				
+				stage.show();
+							
+				priorStage.close();	
+	
+				
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+    		
+    	}catch(Exception e) {
+    		
+    		label.setText( "ERROR ! ALGUNO DE LOS DATOS ESTA MAL INGRESADO" );
+    		
+    	}
 
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     }	
  	
 }
--- src/application/controlador/CRegistrarSeguridad.java
+++ src/application/controlador/CRegistrarSeguridad.java
@@ -15,6 +15,8 @@ import java.time.LocalDate;
 import java.util.Vector;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+
+import application.controlador.excepciones.DatosVacios;
 import application.model.Seguridad;
 
 public class CRegistrarSeguridad {
@@ -85,69 +87,80 @@ public class CRegistrarSeguridad {
     
     @FXML
     void registrarse(ActionEvent event) {
-    	String email = jfxEmail.getText();
-    	String nombre = jfxNombre.getText();
-    	String apellido = jfxApellido.getText();
-    	String password = jfxPassword.getText();
-    	int telefono = Util.parsearInt(jfxTelefono.getText());
-    	String sexo = jfxSexo.getText();
-    	String domicilio = jfxDomicilio.getText();
-    	String zonaControla = jfxZonaControla.getText();
-
-
-		Seguridad seguridad = new Seguridad( email , nombre , apellido , password ,telefono , domicilio , sexo , zonaControla , LocalDate.now());
-		Seguridad seguridad1 = new Seguridad( "flabio.rodriguez@example.com" , "Flabio" , "Rodriguez" , "seguridad24" , 652789254 , "Glorieta Loreto Nicolás 6, Ceuta, 04195" , "M" ,"Edificio A , planta B" , LocalDate.now());
-		Seguridad seguridad2 = new Seguridad( "santiago.martinez@example.com" , "Santiago" , "Martinez" , "nosequeponer12" , 658587235 , "Calle Ángeles Mendizábal 7 Apt. 91, Cádiz, 24435" , "M" ,"Edificio A , comedor" , LocalDate.now());
-		Seguridad seguridad3 = new Seguridad( "nicolas.fernandez@example.com" , "Nicolas" , "Fernandez" , "segurata4life" , 651027841 , "Pasadizo de Eduardo Baños 5 Piso 3, Ourense, 30825" , "M" , "Edificio B, patio afuera" ,LocalDate.now());
-    	Vector<Seguridad> seguridades = new Vector<Seguridad>();
-    	seguridades.add(seguridad);
-    	seguridades.add(seguridad1);
-    	seguridades.add(seguridad2);
-    	seguridades.add(seguridad3);
-		
-		Gson prettyGson = new GsonBuilder().setPrettyPrinting().create();
-		String representacionBonita = prettyGson.toJson(seguridad);
-		System.out.println(representacionBonita);
-		
-		
-		try(FileWriter writer = new FileWriter("src/application/model/json/seguridad.json")){
-			
-			prettyGson.toJson( seguridades , writer);
-			
-	    } catch (IOException e) {
-	    	
-	        e.printStackTrace();
-	        
-	    }
-    
-    	// ---------------------------Forma generica de llamar a una nueva ventana desde otra, es lo mismo siempre, tengo que cambiar los nombre de los paramentos nada mas, y el path del get source.---------------------------
-       	//Registrar al cliente, y mandarlo al iniciar sesion asi puede entrar.
-    	
-    	Stage priorStage = (Stage)btnVolverAlMenu.getScene().getWindow();
-    	Stage stage = new Stage();
-
     	try {
- 
-			FXMLLoader loader8 = new FXMLLoader(getClass().getResource("/application/view/IniciarSesion.fxml")); //Cargo el loader
+    		
+	    	String email = jfxEmail.getText();
+	    	String nombre = jfxNombre.getText();
+	    	String apellido = jfxApellido.getText();
+	    	String password = jfxPassword.getText();
+	    	int telefono = Util.parsearInt(jfxTelefono.getText());
+	    	String sexo = jfxSexo.getText();
+	    	String domicilio = jfxDomicilio.getText();
+	    	String zonaControla = jfxZonaControla.getText();
+	
+	
+	    	if (Util.stringVacio(email) || Util.stringVacio(nombre) || Util.stringVacio(apellido) || Util.stringVacio(password) || Util.stringVacio(sexo) || Util.stringVacio(domicilio) || telefono == 0 || Util.stringVacio(zonaControla)){
+		    	
+	    		throw new DatosVacios("Alguno de los datos esta mal");
+	    	}
+	    	
+			Seguridad seguridad = new Seguridad( email , nombre , apellido , password ,telefono , domicilio , sexo , zonaControla , LocalDate.now());
+			Seguridad seguridad1 = new Seguridad( "flabio.rodriguez@example.com" , "Flabio" , "Rodriguez" , "seguridad24" , 652789254 , "Glorieta Loreto Nicolás 6, Ceuta, 04195" , "M" ,"Edificio A , planta B" , LocalDate.now());
+			Seguridad seguridad2 = new Seguridad( "santiago.martinez@example.com" , "Santiago" , "Martinez" , "nosequeponer12" , 658587235 , "Calle Ángeles Mendizábal 7 Apt. 91, Cádiz, 24435" , "M" ,"Edificio A , comedor" , LocalDate.now());
+			Seguridad seguridad3 = new Seguridad( "nicolas.fernandez@example.com" , "Nicolas" , "Fernandez" , "segurata4life" , 651027841 , "Pasadizo de Eduardo Baños 5 Piso 3, Ourense, 30825" , "M" , "Edificio B, patio afuera" ,LocalDate.now());
+	    	Vector<Seguridad> seguridades = new Vector<Seguridad>();
+	    	seguridades.add(seguridad);
+	    	seguridades.add(seguridad1);
+	    	seguridades.add(seguridad2);
+	    	seguridades.add(seguridad3);
 			
-			CIniciarSesion controlador8 = new CIniciarSesion(); //creo el controlador
-		
-			loader8.setController(controlador8); //seteo el controlador con el loader que cree antes.
+			Gson prettyGson = new GsonBuilder().setPrettyPrinting().create();
+			String representacionBonita = prettyGson.toJson(seguridad);
+			System.out.println(representacionBonita);
 			
-			Parent root8 = loader8.load(); //lo pongo como parent
 			
-			Scene scene = new Scene( root8 );
+			try(FileWriter writer = new FileWriter("src/application/model/json/seguridad.json")){
+				
+				prettyGson.toJson( seguridades , writer);
+				
+		    } catch (IOException e) {
+		    	
+		        e.printStackTrace();
+		        
+		    }
+	    
+	    	// ---------------------------Forma generica de llamar a una nueva ventana desde otra, es lo mismo siempre, tengo que cambiar los nombre de los paramentos nada mas, y el path del get source.---------------------------
+	       	//Registrar al cliente, y mandarlo al iniciar sesion asi puede entrar.
+	    	
+	    	Stage priorStage = (Stage)btnVolverAlMenu.getScene().getWindow();
+	    	Stage stage = new Stage();
+	
+	    	try {
+	 
+				FXMLLoader loader8 = new FXMLLoader(getClass().getResource("/application/view/IniciarSesion.fxml")); //Cargo el loader
+				
+				CIniciarSesion controlador8 = new CIniciarSesion(); //creo el controlador
 			
-			stage.setScene(scene);
-			
-			stage.show();
-						
-			priorStage.close();	
+				loader8.setController(controlador8); //seteo el controlador con el loader que cree antes.
+				
+				Parent root8 = loader8.load(); //lo pongo como parent
+				
+				Scene scene = new Scene( root8 );
+				
+				stage.setScene(scene);
+				
+				stage.show();
+							
+				priorStage.close();	
+	
+				
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+    		
+    	}catch (Exception e) {
+    		label.setText( "ERROR ! ALGUNO DE LOS DATOS ESTA MAL INGRESADO" );
+    	}
 
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     } 	
 }
